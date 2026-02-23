48b39ca6ffe55cc35d5a31abfacfe4de29bcbe13
ecerci-atilim <107133826+ecerci-atilim@users.noreply.github.com>
2026-02-23 15:31:55 +0300

    Update schedule-config.js

--- schedule-config.js
+++ schedule-config.js
@@ -2,18 +2,18 @@
 
 window.scheduleData = {
     monday: {
+        "11:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
         "11:30": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
-        "12:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
     },
     tuesday: {
         "11:30": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
         "12:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
-        "12:30": { activity: "EE352 Laboratory (S1)", location: "B4015", color: "rgb(250, 255, 175)"},
-        "13:00": { activity: "EE352 Laboratory (S1)", location: "B4015", color: "rgb(250, 255, 175)"},
+        "11:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
+        "11:30": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
     },
     wednesday: {
+        "11:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
         "11:30": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
-        "12:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
         "13:30": { activity: "EE214 Laboratory (S1)", location: "B2015", color: "#deacffff"},
         "14:00": { activity: "EE214 Laboratory (S1)", location: "B2015", color: "#deacffff"},
         "14:30": { activity: "EE214 Laboratory (S2)", location: "B2015", color: "rgb(250, 92, 255)"},
@@ -22,12 +22,12 @@ window.scheduleData = {
     thursday: {
         "09:30": { activity: "EE316 Laboratory (S1)", location: "B2013", color: "#b2ffd2"},
         "10:00": { activity: "EE316 Laboratory (S1)", location: "B2013", color: "#b2ffd2"},
+        "11:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
         "11:30": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
-        "12:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
     },
     friday: {
+        "11:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
         "11:30": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
-        "12:00": { activity: "Lunch Break", location: "Out of office", color: "rgb(173, 173, 173)"},
         "12:30": { activity: "EE352 Laboratory (S2)", location: "B4015", color: "#rgb(244, 255, 91)"},
         "13:00": { activity: "EE352 Laboratory (S2)", location: "B4015", color: "#rgb(244, 255, 91)"},
         "14:30": { activity: "EE316 Laboratory (S2)", location: "B2013", color: "#46ff93"},
